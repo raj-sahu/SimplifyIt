69142cb62bb9b7f286216ef621875079411e1e32
raj-sahu <be10363.18@bitmesra.ac.in>
2020-10-08 13:25:02 +0530

    Structured

--- Simlify.cpp
+++ Simlify.cpp
@@ -1,28 +1,43 @@
 #include "script.cpp"
+
 int main()
 {
     // FAST
     cout << "\t\t\tSimplifyTranscations\n\n\n\n";
-    int stop = 0, id = 0, money;
+    int stop = 0, id = 0, Amount;
     string sender, receiver;
     unordered_map<string, int> nameToId;
+    vector<string> idToName;
+
     cout << "\n\t\tENTER TRANSCATIONS : PAYER_NAME RECIVER_NAME MONEY\n \n";
-    vector<Transaction> transcations;
+    vector<Transaction> transcations, simplifiedTranscations;
+    Transaction t;
     do
     {
-        cin >> sender >> receiver >> money;
+        cin >> sender >> receiver >> Amount;
         if (nameToId.count(sender) == 0)
+        {
             nameToId[sender] = id++;
+            idToName.push_back(sender);
+        }
         if (nameToId.count(receiver) == 0)
+        {
             nameToId[receiver] = id++;
-        transcations.emplace_back(nodeToId[sender], nodeToId[receiver], money);
-        cout << "ENTER 1 TO ADD MORE TRANSCATIONS";
-    } while (stop != 1);
+            idToName.push_back(receiver);
+        }
+        t.payer = nameToId[sender];
+        t.receiver = nameToId[receiver];
+        t.money = Amount;
+        transcations.push_back(t);
+        cout << "ENTER 1 TO ADD MORE TRANSCATIONS\t";
+        cin >> stop;
+        cout << endl;
+    } while (stop == 1);
 
-    solve(transcations);
+    simplifiedTranscations = solve(transcations);
     for (Transaction x : transcations)
     {
-        cout << nameToId[x.payer] << "->" << nameToId[x.receiver] << " Rs " < < < x.money << endl;
+        cout << idToName[x.payer] << "->" << idToName[x.receiver] << " Rs " << x.money << endl;
     }
     return 0;
 }
--- script.cpp
+++ script.cpp
@@ -7,13 +7,8 @@ using namespace std;
 struct Transaction
 {
     int payer, receiver, money;
-    Transcation(int p, int r, int m) : payer(p), receiver(r), money(m){};
 };
 
-void solve(vector<Transaction> &transcations)
+vector<Transaction> solve(vector<Transaction> &transcations)
 {
-    for (Transaction x : transcations)
-    {
-        cout << x.payer << "->" << x.receiver << " Rs " < < < x.money << endl;
-    }
 }
